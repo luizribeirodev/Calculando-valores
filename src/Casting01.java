814a9690d223adbff1bbb0e2f61584d3375aa525
Aluno <Aluno@LAB04-PC05>
2024-06-11 21:28:24 -0300

    Verificando valores

---
+++ src/Casting01.java
@@ -0,0 +1,24 @@
+public class Casting01 {
+    public static void main(String[] args) {
+        //Declara e inicializa um byte com valor 128
+        //Observe a reclamação do NetBeans
+        
+        
+        //Declara e inicializa um short com valor 128
+        //Cria uma instrução print que transforma esse short em um byte
+
+        
+        float number = 128;
+        byte number1 = 127;
+        number1 += 1;
+        System.out.println(number);
+        System.out.println(number1);
+
+        //Declara e inicializa um byte com valor 127
+        //Adicione 1 a esta variável e imprima-a
+        //Adicione 1 a esta variável novamente e imprima-a novamente
+        
+        
+        
+    }    
+}
